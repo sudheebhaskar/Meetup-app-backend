868fcf07bbe04b52e40a662076e6f43b541140a5
Sudheendra Bhaskar <sudheebhaskar@gmail.com>
2025-02-12 16:23:44 +0530

    chore: modifications done

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI;
+// const mongoURI = process.env.MONGODB_URI;
 
   
+// const initializeDatabase = async () => {
+//   try{
+//     const connection = await mongoose.connect(mongoURI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     if(connection){
+//       console.log('Connected Successfully');
+//     }
+//   } catch(error){
+//     console.log('Connection Failed', error);
+//   }
+// }
+
+// module.exports = { initializeDatabase };
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const initializeDatabase = async () => {
-  try{
-    const connection = await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    if(connection){
-      console.log('Connected Successfully');
-    }
-  } catch(error){
-    console.log('Connection Failed', error);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
   }
-}
-
-module.exports = { initializeDatabase };
-
+};
--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
+require('dotenv').config();
+
+app.use(cors());
+
 app.use(cors(corsOptions));
 
 const jsonData = fs.readFileSync('events.json', 'utf8')
@@ -75,4 +79,7 @@ const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server is running on this port: ${PORT}`)
 })
+
+
+
 
